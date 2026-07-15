a1f3a3bccbc3233984b351e9b7263add96fadf66
Jeeva <jeevai1806@gmail.com>
2026-07-15 15:37:26 +0000

    GCD using the while loop

---
+++ c_template/Loop/gcd.c
@@ -0,0 +1,25 @@
+/************************************************************************
+Name: Jeeva
+Date: 15/07/2026
+Description: GCD using the while loop
+************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, temp;
+
+    scanf("%d%d", &a, &b);
+
+    while (b != 0)
+    {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("%d", a);
+
+    return 0;
+}

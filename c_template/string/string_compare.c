ecaa7f88917c0ee9ef00e1d33887dfb2bb6eb0e9
Jeeva <jeevai1806@gmail.com>
2026-07-22 14:40:59 +0000

    Using strcmp inbuild function

---
+++ c_template/string/string_compare.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str1[50], str2[50]; //Declaration
+
+    printf("Enter string1 : ");
+    fgets(str1,50,stdin); //Getting string 2
+    
+    printf("Enter string2 : ");
+    fgets(str2,50,stdin); //getting string 2
+    int ret = strcmp(str1, str2); //Using string comparision
+    printf("The value of strcmp: %d\n", ret); //printing compraion result 
+    ret == 0 ? printf("Strings are equal\n") : printf("Strings are not equal\n"); //Using ternary condition 
+
+    return 0;
+}

4eabe3cfbd19ece3d9f50fb44bd9815d3722cd25
Jeeva <jeevai1806@gmail.com>
2026-07-23 15:37:34 +0000

    Getting both string and delimeter(separator) parameter from the user

--- c_template/string/strtok.c
+++ c_template/string/strtok.c
@@ -2,13 +2,22 @@
 strtok() --> It will divide the given string into small sub string depending on the delimeter(separator)
 */
 
+//HeaderInclusion
 #include<stdio.h>
 #include<string.h>
 
+//Main program starts here()
 int main()
 {
-    char str[] = "Bengaluru Chennai Mumbai delhi";
-    char delim[] = " ";
+    //Declaration
+    char str[50];
+    char delim[20];
+
+    //Getting input from user
+    printf("Enter the strings : ");
+    fgets(str, 50 , stdin);
+    printf("Enter the delimeter parameters: ");
+    fgets(delim,20,stdin);
 
     char *token = strtok(str, delim);
     while(token != NULL)

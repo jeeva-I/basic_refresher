b2c8e6c6c557948feb61dc8b3b2b7d71b23c73b4
Jeeva <jeevai1806@gmail.com>
2026-08-05 14:35:48 +0000

    Using width specifier(for decoration of project)

---
+++ c_template/stdio/width.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int num = 10;
+    printf("%5d\n", num);
+
+    printf("******************************************************\n");
+    printf("%5s%7s%10s%10s%12s\n", "No.", "Name","MobileNO", "Email Id","Address");
+    printf("******************************************************\n");
+
+
+    return 0;
+}

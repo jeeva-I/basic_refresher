7fc2eb3c21205997c234e8006ce4010ece418db0
Jeeva <jeevai1806@gmail.com>
2026-08-05 15:20:44 +0000

    Rerturning the printf() function -- even interger also consider as character by defalult

--- c_template/stdio/carriage_return.c
+++ c_template/stdio/carriage_return.c
@@ -2,5 +2,14 @@
 
 int main()
 {
-    printf("Hello guys\r hi frds\n");
+    //printf("Hello guys\r hi frds\n");
+    char str[] = "Hello World";
+
+    printf("%zu\n", sizeof(str));
+
+    int ret = printf("%s-%d\n", str, 100);
+
+    printf("The returning of printf is %d ", ret);
+
+    return 0;
 }

d0ef04f510adb4415e54deec2653b4ccc88f76e2
Jeeva <jeevai1806@gmail.com>
2026-08-06 15:17:38 +0000

    /Using \n to avoid the bufffering

--- c_template/stdio/buffer1.c
+++ c_template/stdio/buffer1.c
@@ -3,9 +3,10 @@
 
 int main()
 {
+    
     while(1)
     {
-        printf("Hello World");
+        printf("Hello World\n"); //Using \n to avoid the bufffering 
         sleep(1); //it displays the output when the buffer memory gets full
     }
 

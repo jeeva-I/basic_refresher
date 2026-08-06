7492a99468dcf05518ac8427c3b1c9e9ef831ebd
Jeeva <jeevai1806@gmail.com>
2026-08-06 15:20:39 +0000

    setbuf() --> used to avoid buffer only but it is not recommented

--- c_template/stdio/buffer1.c
+++ c_template/stdio/buffer1.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-    
+    setbuf(stdout,NULL); //This method is not recommented
     while(1)
     {
-        printf("Hello World\n"); //Using \n to avoid the bufffering 
+        printf("Hello World"); //Using \n to avoid the bufffering 
         sleep(1); //it displays the output when the buffer memory gets full
     }
 

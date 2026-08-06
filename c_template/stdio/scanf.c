385176d16c6363d033cb20212b007fbd5b8cb1fb
Jeeva <jeevai1806@gmail.com>
2026-08-06 15:44:10 +0000

    using buffer for taking the input automatically

--- c_template/stdio/scanf.c
+++ c_template/stdio/scanf.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
 
+
 int main()
 {
     int num1; 
     char ch;
     int num2;
     char str[10];
+    char name[20];
 
     scanf("%d %c %d %s", &num1, &ch, &num2, str);
     printf("%d %c %d %s\n", num1, ch, num2, str);
-     // try with 10.5 a 
+     // try with 10.5 a emertxe
+
+     printf("Enter the name : "); //it will automatically taking the input fromt the buffer
+     scanf("%s", name);
+
+     printf("Name is %s\n", name);
+
     return 0;
 }

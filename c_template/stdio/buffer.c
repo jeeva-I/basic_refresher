4448d30e648646f2e557092deee84df768f52070
Jeeva <jeevai1806@gmail.com>
2026-08-06 14:19:59 +0000

    Using buffer

---
+++ c_template/stdio/buffer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1; 
+    char ch;
+    int num2;
+    char str[10];
+
+    scanf("%d %c %d %s", &num1, &ch, &num2, str);
+    printf("%d %c %d %s\n", num1, ch, num2, str);
+     // try with 10.5 a 
+    return 0;
+}

b3875941a24dd0f61b81abb0f7cfdd2ec9a69a4a
Jeeva <jeevai1806@gmail.com>
2026-08-18 06:16:17 +0000

    Defrencing pointer using type conversion

--- c_template/pointer2/void.c
+++ c_template/pointer2/void.c
@@ -7,7 +7,8 @@ int main()
     void *iptr = &num;
     void *cptr = &ch;
 
-    printf("%d\n", *iptr); //Trying to derefrencing
+    printf("%d\n", *(int *)iptr); //Trying to derefrencing using type conversion
+    printf("%c\n", *(char *)cptr);
 
 
     // printf("Size of void pointer : %zu\n", sizeof(iptr));

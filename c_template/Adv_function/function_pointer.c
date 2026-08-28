d2c0d7daf8c7ed5c10b953ceedd27b6cfc7c976e
Jeeva <jeevai1806@gmail.com>
2026-08-28 14:44:49 +0000

    Finding the size of function pointer while passing and normal

--- c_template/Adv_function/function_pointer.c
+++ c_template/Adv_function/function_pointer.c
@@ -9,8 +9,10 @@ int main()
     int (*fptr)(int,int) = &sum; //Function pointer used to store address of the function
     int res = (*fptr)(10,10); //Function call through funciton pointer
 
-    printf("Res : %d\n", res); 
-    printf("Direct: %d\n", fptr(30,20)); //Printing directly using function pointer
+    // printf("Res : %d\n", res); 
+    // printf("Direct: %d\n", fptr(30,20)); //Printing directly using function pointer
+    printf("size of function pointer: %zu\n", sizeof(fptr)); //sizeof(fptr) --> 8 bytes
+    printf("size of function pointer: %zu\n", sizeof(fptr(10,20))); //sizeof(int) --> 4 bytes
 
     return 0;
 }

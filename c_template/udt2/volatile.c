59875962cda899d974d0cdf03b73039adb4a649e
Jeeva <jeevai1806@gmail.com>
2026-09-02 14:30:32 +0000

    optimizing code without volatile deleteing loop

---
+++ c_template/udt2/volatile.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+     long int wait;
+    unsigned char bit = 0;
+    while(1)
+    {
+        bit = !bit;
+        printf("The bit is now : %d\r", bit);
+        fflush(stdout);
+        for(wait = 0xffffffff;wait--;);
+    }
+
+    return 0;
+}

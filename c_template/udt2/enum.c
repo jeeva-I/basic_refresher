c92a50ad9be69ba27bf86eae464fa0fad517c1be
Jeeva <jeevai1806@gmail.com>
2026-09-02 14:12:32 +0000

    printing the size of enum

--- c_template/udt2/enum.c
+++ c_template/udt2/enum.c
@@ -3,13 +3,16 @@
 enum bool
 {
     e_true,
-    e_flase
+    e_flase,
+    e_next
 };
 
 int main()
 {
     printf("%d\n", e_true);
     printf("%d\n", e_flase);
+    printf("%d\n", e_next);
+    printf("Size of enum : %zu\n", sizeof(enum bool));
 
     return 0;
 }
